55f68ba6bfa729d6546279b11c3c204c0eedf708
Prabha Rashmi <rashmiprabha1198@gmail.com>
2020-12-09 16:16:22 +0530

    test

--- src/main/java/com/example/demo/repository/WorkOrdersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Workorders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkOrdersRepository extends JpaRepository<Workorders,String > {
-    public void deleteByInspplanidAndFormid(String inspid, int formid);
-    public void deleteByFormid(int formid);
-}
